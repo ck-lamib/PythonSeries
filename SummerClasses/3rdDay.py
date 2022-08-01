e3fa3774e47a136e1967208ae3b329ed5d452cb5
ck-lamib <113ck.lamib@gmail.com>
2022-08-01 19:35:24 +0545

    adding 3rdDay.py file in SummerClasses

---
+++ SummerClasses/3rdDay.py
@@ -0,0 +1,50 @@
+#list[accessing element, replace, insert, sort,  pop, loop]
+
+m = int(input("Enter row: "))
+n = int(input("Enter column: "))
+print("it will be "+ "["+ str(m)+"]"+"["+ str(n)+"] matrix")
+row1 = []
+print("Enter details for first "+ "["+ str(m)+"]"+"["+ str(n)+"] matrix")
+for i in range(m):
+    column = []
+    for j in range(n):
+        a= int(input("enter number for "+"["+ str(i)+"]"+"["+ str(j)+"] :"))
+        column.append(a)
+    row1.append(column)
+
+row2 = []
+print("Enter details for second "+ "["+ str(m)+"]"+"["+ str(n)+"] matrix")
+for i in range(m):
+    column = []
+    for j in range(n):
+        a= int(input("enter number for "+"["+ str(i)+"]"+"["+ str(j)+"] :"))
+        column.append(a)
+    row2.append(column)
+
+sum =[]
+for i in range(m):
+    sumCol = []
+    for j in range(n):
+        a = row1[i][j]
+        b = row2[i][j]
+        c = a+b
+        sumCol.append(c)
+    sum.append(sumCol)
+
+print("first matrix is : ")
+for i in range(m):
+    for j in range(n):
+        print(row1[i][j], " ", end="")
+    print()
+
+print("second matrix is : ")
+for i in range(m):
+    for j in range(n):
+        print(row2[i][j], " ", end="")
+    print()
+
+print("sum matrix is : ")
+for i in range(m):
+    for j in range(n):
+        print(sum[i][j], " ", end="")
+    print()
